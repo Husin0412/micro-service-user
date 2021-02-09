b3ec009d8a7630ebd73726d1e7f5d6526f0c1b1a
Husin <Muhamad.husin0412@gmail.com>
2021-02-09 13:44:03 +0700

    fix

--- routes/handler/users/getUsers.js
+++ routes/handler/users/getUsers.js
@@ -4,9 +4,9 @@ module.exports = async (req, res) => {
 
     const userIds = req.query.user_ids || [];
 
-    // const sqlOptions = {
-    //     attributes: ['id', 'guid', 'name', 'email', 'role', 'profession', 'avatar', 'created_at', 'updated_at']
-    // }
+    const sqlOptions = {
+        attributes: ['id', 'guid', 'name', 'email', 'role', 'profession', 'avatar', 'created_at', 'updated_at']
+    }
 
     if(userIds.length) {
         sqlOptions.where = {
